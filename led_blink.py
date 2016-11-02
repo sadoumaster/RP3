df8831c76da8cdd84ff6bcaa413345672a5a8bee
SANHU-10 <user1@SANHU10.local>
2016-11-03 00:43:15 +0900

    new files

---
+++ led_blink.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  3 00:21:46 2016
+
+@author: user1
+"""
+
+import RPi.GPIO as Gpio #Import raspberry pi library
+import time #for waiting
+
+Gpio.setmode(Gpio.BCM)
+Gpio.setup(2, Gpio.OUT)
+count=0
+
+while count < 50 :
+    Gpio.output(2, Gpio.HIGH)
+    time.sleep(0.5)
+    Gpio.output(2, Gpio.LOW)
+    time.sleep(0.5)
+    count += 1
