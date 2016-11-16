4ab8f6045b4381441f0de07b46198946dd484ddb
sadoumaster <sadoumaster@hotmail.com>
2016-11-17 00:22:39 +0900

    LED ver1

--- LED/LED.py
+++ LED/LED.py
@@ -8,13 +8,15 @@ led_blink program
 
 import RPi.GPIO as GPIO #Import raspberry pi library
 import time #for waiting
-GPIO.setmode(GPIO.BCM)
+import math
+#GPIO.setmode(GPIO.BCM)
 
 #LEDの点灯パターンを記したclass。LED(num) numにGPIO PINを指定。
 #with as 構文使用可能。ex. with LED(4) as redLED :
 class LED():
     def __init__(self, GPIO_PIN_No):
         self.GPIO_PIN = GPIO_PIN_No
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.GPIO_PIN, GPIO.OUT)
         print ('set % PIN for LED'.format(self.GPIO_PIN))
     
@@ -25,20 +27,20 @@ class LED():
             GPIO.output(self.GPIO_PIN, GPIO.LOW)
             time.sleep(interval)
 
-    def pwm(self, power = 100, time = 0.1, Hz = 50):
+    def pwm(self, power = 100, sec = 0.1, Hz = 100):
         if (power < 0) & (power > 100):
             print('Input power between 0 and 100')
             pass
         if Hz < 1 :
             print('input Hz bigger than 1')
             pass
-        if time < 0 :
+        if sec < 0 :
             print('input time bigger than 0')
             pass
         interval = 1 / Hz
         ontime = interval * power / 100
         offtime = interval - ontime
-        for i in range(time * Hz):
+        for i in range(math.ceil(sec * Hz)):
             GPIO.output(self.GPIO_PIN, GPIO.HIGH)
             time.sleep(ontime)
             GPIO.output(self.GPIO_PIN, GPIO.LOW)
--- LED/example/led_example.py
+++ LED/example/led_example.py
@@ -6,32 +6,31 @@ Created on Thu Nov 10 13:05:54 2016
 """
 import sys
 sys.path.append('/home/pi/Documents/PythonProjects')
-import RP3.LED as LED
+import RP3.LED.LED as LED
 import time, math
 
-def sin_curve_pwm():    
+def sin_curve_pwm(times=1 ,circle_sec=10):    
     with LED.LED(4) as Blueled:
-        Blueled.blink(times=10)
-        time.sleep(0.5)
-        for i in range(101):
-            Blueled.pwm(power = i)
-            time.sleep(0.1)
-        time.sleep(0.5)
-        for i in range(-90, 271):
-            degrees = math.pi()/i
-            Blueled.pwm(power = (math.sin(degrees)+1)*100, time=0.05)
+        for j in range(times):
+            for i in range(-90, 271):
+                degrees = 2 * math.pi/360 * i
+                Blueled.pwm(power = (math.sin(degrees)+1)*100/2, sec=0.05)
+
         print ('end')
 
 def blink_1f_pwm():
     from RP3.curve1f import curve_1f_generator
     with LED.LED(4) as Blueled:
         while True:
-            fluctuation = curve_1f_generator()#-1.5~1.5の数字
+            fluctuation = curve_1f_generator(resolution=500)#-1.5~1.5の数字
             try:
                 while True:
-                    Blueled.pwm(power = ((fluctuation.__next__()+1.5)/3*100), 
-                                time=0.5)
+                    a=fluctuation.__next__()
+                    #print((a+1.5)/3*100)
+                    Blueled.pwm(power = ((a+1.5)/3*100), 
+                                sec=0.01)
             except StopIteration:
                 continue
-        
-                
+if __name__ == '__main__':
+    sin_curve_pwm(2)
+    blink_1f_pwm()
