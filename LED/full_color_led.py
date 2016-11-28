1a7ffd21ab25c15360fc9d9abf63e7c91593679e
SANHU-10 <user1@SANHU10.local>
2016-11-28 13:04:02 +0900

    add fullcolorLED

--- LED/full_color_led.py
+++ LED/full_color_led.py
@@ -24,10 +24,14 @@ def reset():
     sleep(0.00005)
 
 def send_bit(color):
+    #色でbit_dataを変える。
     if   color=='R': bit_data=R_led
     elif color=='G': bit_data=G_led
     elif color=='B': bit_data=B_led
     elif color==0: bit_data=0
+    #bit_dataが0−255になければ、０か２５５にする。
+    if bit_data < 0: bit_data=0
+    elif bit_data >255: bit_data=255
     for i in range(8):
         if bit_data & 0x80:
             send_1()
@@ -53,5 +57,21 @@ GPIO.setup(data_pin, GPIO.OUT)
 
 try:
     on()
+    sleep(5)
+    off()
+    sleep(1)
+    for i in range(255):
+        R_led, G_led, B_led=i, 0, 0
+        on()
+        sleep(0.02)
+    for i in range(255):
+        R_led, G_led, B_led=0, i, 0
+        on()
+        sleep(0.02)
+    for i in range(255):
+        R_led, G_led, B_led=0, 0, i
+        on()
+        sleep(0.02)
+    off()
 except KeyboardInterrupt:
     GPIO.cleanup()
