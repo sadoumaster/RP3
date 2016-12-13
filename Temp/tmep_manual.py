37526906407b643c4c9c5ea7cf8fbf5cd77d3377
SANHU-10 <user1@SANHU10.local>
2016-12-13 16:03:05 +0900

    add temp

--- Temp/tmep_manual.py
+++ Temp/tmep_manual.py
@@ -14,17 +14,31 @@ import RPi.GPIO as GPIO
 
 R, G, B=21, 22, 23
 with LED(R) as R, LED(G) as G, LED(B) as B:
-    def safe_temp():
+    def safe_tempUP():
+        pass
+
+    def safe_tempDOWN():
         pass
     
-    def near_temp():
+    def near_tempUP():
         pass
     
-    def over_temp():
+    def near_tempDOWN():
         pass
+
+    def over_tempUP():
+        pass
+
+    def over_tempDOWN():
+        pass
+
     
-    def too_over_temp():
+    def too_over_tempUP():
         pass
+
+    def too_over_tempDOWN():
+        pass
+
     
     def off():
         R.off()
@@ -34,20 +48,33 @@ with LED(R) as R, LED(G) as G, LED(B) as B:
     def main():
         now_temp=0
         if len(sys.argv) <2:
-            print('usafge: {} Target_Temperature'.format(sys.argv[0]))
+            print('usage: {} Target_Temperature'.format(sys.argv[0]))
             sys.exit(0)
         target = float(sys.argv[1])
+        now_temp_archive=0
         while True:
             now_temp=read_temp()
             diff_temp=target-now_temp
-            if diff_temp > 2.5:
-                safe_temp()
-            elif -2.5 <=diff_temp <= 2.5:
-                near_temp()
-            elif -7.5 <=diff_temp < -2.5:
-                over_temp()
-            else:
-                too_over_temp()
+            if now_temp_archive < now_temp:                    
+                if diff_temp > 2.5:
+                    safe_tempUP()
+                elif -2.5 <=diff_temp <= 2.5:
+                    near_tempUP()
+                elif -7.5 <=diff_temp < -2.5:
+                    over_tempUP()
+                else:
+                    too_over_tempUP()
+                    
+            else:                    
+                if diff_temp > 2.5:
+                    safe_tempDOWN()
+                elif -2.5 <=diff_temp <= 2.5:
+                    near_tempDOWN()
+                elif -7.5 <=diff_temp < -2.5:
+                    over_tempDOWN()
+                else:
+                    too_over_tempDOWN()
+            now_temp_archive=now_temp
             sleep(5)
     
     if __name__=='__main__':
